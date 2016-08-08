9e18a96b83202de1fcc95b2c5daa102136874f6f
viniciusksilva <viniciusksilva@gmail.com>
2016-08-08 15:59:47 -0300

    ISSUE 865 constroladores e serviços separados. Tela de distribuicao comum separada

--- src/main/java/br/jus/stf/autuacao/distribuicao/interfaces/DistribuicaoRestResource.java
+++ src/main/java/br/jus/stf/autuacao/distribuicao/interfaces/DistribuicaoRestResource.java
@@ -2,12 +2,14 @@ package br.jus.stf.autuacao.distribuicao.interfaces;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.jus.stf.autuacao.distribuicao.application.DistribuicaoApplicationService;
 import br.jus.stf.autuacao.distribuicao.application.commands.DistribuirProcessoCommand;
+import br.jus.stf.autuacao.distribuicao.domain.ProcessoAdapter;
+import br.jus.stf.autuacao.distribuicao.domain.model.Distribuicao;
+import br.jus.stf.autuacao.distribuicao.domain.model.DistribuicaoId;
+import br.jus.stf.autuacao.distribuicao.domain.model.DistribuicaoRepository;
 import br.jus.stf.autuacao.distribuicao.domain.model.TipoDistribuicao;
+import br.jus.stf.autuacao.distribuicao.interfaces.dto.ProcessoDto;
 import br.jus.stf.autuacao.distribuicao.interfaces.dto.TipoDistribuicaoDto;
+
+import br.jus.stf.core.shared.processo.ProcessoId;
+import br.jus.stf.core.shared.processo.TipoProcesso;
 
 /**
  * @author Rodrigo Barreiros
@@ -31,6 +41,11 @@ public class DistribuicaoRestResource {
     @Autowired
     private DistribuicaoApplicationService distribuicaoApplicationService;
     
+    @Autowired
+    private DistribuicaoRepository distribuicaoRepository;
+    
+    @Autowired
+    private ProcessoAdapter processoAdapter;
 
     /**
      * @param command
@@ -52,6 +67,19 @@ public class DistribuicaoRestResource {
     public List<TipoDistribuicaoDto> listarTiposDistribuicao(){
 		return Arrays.asList(TipoDistribuicao.values()).stream().map(tipo -> new TipoDistribuicaoDto(tipo.name(), tipo.exigeJustificativa()))
 				.collect(Collectors.toList());
+    }
+	
+    /**
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}/processo", method = RequestMethod.GET)
+    public ProcessoDto consultarDistribuicao(@PathVariable("id") Long id) {
+    	Optional<Distribuicao> distribuicao =  Optional.ofNullable(distribuicaoRepository.findOne(new DistribuicaoId(id)));
+    	
+    	return Optional.ofNullable(processoAdapter.consultar(distribuicao.get().processo().toLong()))
+    			.orElseThrow(() -> new IllegalArgumentException("Processo inválido."));
+    	
     }
 	
 
--- src/main/ui/app/distribuicao/distribuicao/distribuicao.module.ts
+++ src/main/ui/app/distribuicao/distribuicao/distribuicao.module.ts
@@ -4,6 +4,8 @@ import IModule = angular.IModule;
 import Properties = app.support.constants.Properties;
 import IStateParamsService = angular.ui.IStateParamsService;
 import {DistribuicaoService} from "./distribuicao.service";
+import {DistribuicaoCommonService} from "./commons/distribuicao-common.service";
+
 /*
  * Modulo de configuração do serviço de distribuição de processos.
  * autor: anderson.araujo
@@ -15,31 +17,57 @@ function config($stateProvider: IStateProvider,
                 msNavigationServiceProvider: any,
                 properties: Properties) {
 	
-    /*
-     * properties.apiUrl: retorna o endereço do contexto da aplicação acessado via gateway.
-     * No caso do serviço de distribuição, o endereço é protocolo://docker:porta/distribuicao.
-     * Assim, a localização dos dos arquivos do projeto se dá a partir desse endereço.
-    */
-    $stateProvider.state('app.novo-processo.distribuicao', {
+    $stateProvider.state('app.tarefas.distribuicao', {
         url : '/distribuicao',
         views : {
             'content@app.autenticado' : {
                 templateUrl : "distribuicao.tpl.html",
-                controller : 'app.novo-processo.distribuicao.DistribuicaoController',
+                controller : 'app.distribuicao.DistribuicaoController',
                 controllerAs: 'vm'
-            },
-            resolve : { 
-                tipoDistribuicao: ['app.novo-processo.distribuicao.DistribuicaoService', '$stateParams', (distribuicaoService: DistribuicaoService, $stateParams : IStateParamsService) => {
-                    return distribuicaoService.listarTiposDistribuicao();
-                }],
-                ministrosCandidatos: ['app.novo-processo.distribuicao.DistribuicaoService', '$stateParams', (distribuicaoService: DistribuicaoService, $stateParams : IStateParamsService) => {
-                    return distribuicaoService.listarMinistros();
-                }],
-            },
-            params : {
-                informationId : undefined
             }
+        },
+        resolve : { 
+            tiposDistribuicao: ['app.distribuicao.DistribuicaoService', (distribuicaoService: DistribuicaoService) => {
+                return distribuicaoService.listarTiposDistribuicao();
+            }]
+        },
+        params : {
+            informationId : undefined
         }
+    }).state('app.tarefas.distribuicao-comum', {
+        parent: 'app.tarefas.distribuicao',
+        url: '/comum',
+        views: {
+            '@app.tarefas.distribuicao': {
+                templateUrl: 'comum/distribuicao-comum.tpl.html',
+                controller: 'app.distribuicao.DistribuicaoComumController',
+                controllerAs: 'vm'	
+            }
+        },
+        resolve: {
+        	 ministrosCandidatos: ['app.distribuicao.DistribuicaoCommonService', (distribuicaoCommonService: DistribuicaoCommonService) => {
+                 return distribuicaoCommonService.listarMinistros();
+             }]
+        }
+    }).state('app.tarefas.distribuicao-prevencao', {
+        parent: 'app.tarefas.distribuicao',
+        url: '/prevencao',
+        views: {
+            '@app.tarefas.distribuicao': {
+                templateUrl: 'prevencao/distribuicao-prevencao.tpl.html',
+                controller: 'app.distribuicao.DistribuicaoPrevencaoController',
+                controllerAs: 'vm'
+            }
+        },
+/*        resolve: {
+            processo: ['app.distribuicao.DistribuicaoCommonService', '$stateParams', (distribuicaoCommonService: DistribuicaoCommonService, $stateParams : IStateParamsService) => {
+            	let distribuicaoId = $stateParams['informationId'];
+                return distribuicaoCommonService.consultarProcessoPelaDistribuicao(distribuicaoId);
+            }]
+       }, */ 
+       params : {
+           informationId : undefined
+       }
     });
     
     console.log(properties.apiUrl);
@@ -47,7 +75,7 @@ function config($stateProvider: IStateProvider,
     msNavigationServiceProvider.saveItem('distribuicao', {
         title : 'Distribuição de Processos',
         icon : 'icon-magnify',
-        state : 'app.novo-processo.distribuicao',
+        state : 'app.distribuicao',
         translation : 'DISTRIBUICAO.NOVA',
         weight : 1
     });
@@ -58,7 +86,7 @@ function run($translatePartialLoader: ITranslatePartialLoaderService, properties
     $translatePartialLoader.addPart(properties.apiUrl + '/distribuicao/distribuicao');
 }
 
-let distribuicao: IModule = angular.module('app.novo-processo.distribuicao', ['app.novo-processo', 'app.support', 'ngCookies']);
+let distribuicao: IModule = angular.module('app.distribuicao.distribuicao', ['app.support', 'ngCookies']);
 distribuicao.config(config);
 distribuicao.run(run);
 
