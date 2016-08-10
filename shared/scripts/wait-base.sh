d159f04e501a08485cd8ca3bfacab8c19a3e45fc
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-08-10 01:19:53 -0300

    Removendo shared.'

--- shared/scripts/wait-base.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-./shared/scripts/wait-up.sh "https://docker:8765/info" 300
-./shared/scripts/wait-up.sh "https://docker:8765/discovery/info" 300
-./shared/scripts/wait-up.sh "https://docker:8765/services/info" 300
-./shared/scripts/wait-up.sh "https://docker:8765/userauthentication/info" 150
