1ab35426d1491dec014c8fe615e8dc0ba3df388a
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-08-26 19:23:54 -0300

    Removendo a configuração do mecanismo anterior e adicionando a configuração do novo

--- src/main/java/br/jus/stf/autuacao/distribuicao/interfaces/AutuacaoFinalizadaEventHandler.java
+++ src/main/java/br/jus/stf/autuacao/distribuicao/interfaces/AutuacaoFinalizadaEventHandler.java
@@ -1,13 +1,12 @@
 package br.jus.stf.autuacao.distribuicao.interfaces;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import br.jus.stf.autuacao.distribuicao.application.DistribuicaoApplicationService;
 import br.jus.stf.autuacao.distribuicao.application.commands.IniciarDistribuicaoCommand;
-import br.jus.stf.autuacao.distribuicao.infra.RabbitConfiguration;
 import br.jus.stf.core.shared.eventos.AutuacaoFinalizada;
 
 /**
@@ -26,7 +25,7 @@ public class AutuacaoFinalizadaEventHandler {
     /**
      * @param event
      */
-    @RabbitListener(queues = RabbitConfiguration.AUTUACAO_FINALIZADA_QUEUE)
+    @StreamListener(AutuacaoFinalizada.EVENT_KEY)
     public void handle(AutuacaoFinalizada event) {
         distribuicaoApplicationService.handle(new IniciarDistribuicaoCommand(event.getProcessoId()));
     }
