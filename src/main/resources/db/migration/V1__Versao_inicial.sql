0f73199391ef0ae95dffdf4977bc2a5137572787
Rafael.Alencar <rafaelesdras@gmail.com>
2016-09-13 16:01:41 -0300

    Ajustes na tabela de eventos.

--- src/main/resources/db/migration/V1__Versao_inicial.sql
+++ src/main/resources/db/migration/V1__Versao_inicial.sql
@@ -10,3 +10,5 @@ alter table distribuicao.distribuicao add constraint ck_dist_tip_status check (t
 create table distribuicao.evento (seq_evento number not null, nom_evento varchar2(100) not null, dat_criacao date not null, bin_detalhe clob not null, tip_status smallint not null, constraint pk_evento primary key (seq_evento));
 
 create table distribuicao.distribuicao_evento (seq_distribuicao number not null, seq_evento number not null, constraint pk_distribuicao_evento primary key (seq_distribuicao, seq_evento));
+alter table distribuicao.distribuicao_evento add constraint fk_distribuicao_diev foreign key (seq_distribuicao) references distribuicao.distribuicao(seq_distribuicao);
+alter table distribuicao.distribuicao_evento add constraint fk_evento_diev foreign key (seq_evento) references distribuicao.evento(seq_evento);
